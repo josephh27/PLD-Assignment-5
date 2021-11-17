e511a8e3eadb4c4a91ef6ff66cdcbad9ca647ad0
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-11-17 20:05:20 +0800

    This is the third part of the assignment

---
+++ Assignment_5.2.py
@@ -0,0 +1,19 @@
+running = True
+while running:
+    try:
+        age = int(input("Enter your age: "))
+        if 0 <= age <= 12:
+            print("You are a kid")
+        elif 13 <= age <= 17:
+            print("You are a teen")
+        elif age == 18:
+            print("You are in your debut stage")
+        elif 122 >= age >= 19:
+            print("You are now an adult")
+        elif age > 122:
+            print("Congratulations! You broke the world guinness record for the oldest person to have ever lived!")
+        else:
+            print("You are time travelling")
+        break
+    except:
+        print("Please input a proper number of age")
